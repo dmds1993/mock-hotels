019b16b31319357137002e8522b7ba0b3e6011dd
Daniel Melo <daniel.melo@sciensa.com>
2017-08-03 10:33:02 -0300

    Ajuste endpoint

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,11 @@
 import DS from 'ember-data';
 import config from '../config/environment'
 
-
 export default DS.RESTAdapter.extend({
-  host: config.APP.host
+  host: config.APP.host,
+  header: function() {
+    return {
+      'sec-user-toke': 'dadasdasd'
+    }
+  }
 });
